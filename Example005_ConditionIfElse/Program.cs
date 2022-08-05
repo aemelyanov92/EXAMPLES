6d274505690ec6e9a7939ee4fd9b4e1177841b14
Anatoly Emelyanov <aemelyanov7@gmail.com>
2022-08-05 23:08:33 +0300

    ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "прохор")
+{
+    Console.WriteLine("Прохор, индеец ты такой, ёб твою мать");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
